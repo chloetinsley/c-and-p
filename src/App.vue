0441b66d4511f8bb725a8ef50e8cb926bcc8ec6e
chloe <chloe.tinsley@outlook.com>
2023-06-15 13:45:27 +0100

    message

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.jpeg" class="logo" />
+  <LandingPage msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LandingPage from "./components/LandingPage.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    LandingPage
   }
-}
+};
 </script>
 
 <style>
@@ -22,5 +22,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.logo {
+  height: 150px;
+  width: 150px;
+  /* border-radius: 50%; */
+  /* object-fit: cover; */
+  /* float: left; */
 }
 </style>
---
+++ src/components/LandingPage.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <h1>Phebe and Chloe web design</h1>
+    <div>
+      <p>Our services</p>
+      <ul>
+        <li v-for="v in services" v-bind:key="v">{{v}}</li>
+      </ul>
+    </div>
+    <p>
+      For a guide and recipes on how to configure / customize this project,
+      <br />check out the
+      <a
+        href="https://cli.vuejs.org"
+        target="_blank"
+        rel="noopener"
+      >vue-cli documentation</a>.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LandingPage",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      services: ["Service 1", "Service 2", "Service 3"]
+    };
+  }
+};
+</script>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
